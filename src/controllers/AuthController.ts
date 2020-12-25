27d9b4d9f1f966560d118070639f6ad2180cc2e6
Atreya Bain <christophervrose@gmail.com>
2020-12-25 12:06:13 +0530

    Add todos

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,13 @@
+import baseAPI from '../misc/baseAPI';
+
+// TODO fix
+export default class AuthController {
+    async getOne(req: Request) {
+        return;
+    }
+}
+
+export const api: baseAPI = {
+    pkg: AuthController,
+    methods: [],
+};
