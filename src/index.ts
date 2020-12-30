090ee2df0b71059b1294a6fdfa4f46d65a1c0ee2
Atreya Bain <christophervrose@gmail.com>
2020-12-30 18:21:57 +0530

    Add changes

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ console.log('>Starting Server as main');
 const port = parseInt(process.env.PORT || '3000');
 const res = Application(port, true);
 
-res.then(server => {
+res.then(appliance => {
+    appliance.start();
     console.log('Server running on port:', port);
-    server.start();
 }).catch(err => {
     console.error('E>Could not connect to db:', err?.message ?? err?.msg ?? err?.name ?? 'General error');
 });
